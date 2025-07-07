c13f436a0da5e53361c68cc03952cd93506aca5e
sandeep9896 <pardeepshani61@gmail.co>
2025-07-07 14:49:52 +0530

    succesfull setup server

---
+++ Backend/server.js
@@ -0,0 +1,9 @@
+const http= require('http');
+const app= require('./app');
+const port= process.env.PORT || 3000;
+const server=http.createServer(app);
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+// This code creates an HTTP server using the Express application and listens on the specified port.
+
