b94ee6880d256f104195249d1e38ba59ab3dad86
sandeep9896 <pardeepshani61@gmail.co>
2025-09-20 18:20:47 +0530

    #

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,35 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), // React + Fast Refresh
-    tailwindcss(), // Tailwind CSS
-    visualizer({ open: true }), // Optional: bundle visualizer
-  ],
-
-  // Prevent multiple React instances
-  resolve: {
-    dedupe: ["react", "react-dom"],
-  },
-
-  build: {
-    rollupOptions: {
-      output: {
-        // Custom chunk splitting
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("mapbox-gl")) return "mapbox"; // Mapbox separate chunk
-            return "vendor"; // all other node_modules
-          }
-        },
-      },
-    },
-  },
-
-  optimizeDeps: {
-    include: ["react", "react-dom"], // Ensure single React instance at dev time
-  },
-});
+  plugins: [react(),
+    tailwindcss(),],
+})
